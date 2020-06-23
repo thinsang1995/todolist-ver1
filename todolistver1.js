f71659466169f0c43a4bdb14d6659e5ff965b150
Sang <thinsang1995@gmail.com>
2020-06-23 20:41:40 +0700

    Modify JS

--- todolistver1.js
+++ todolistver1.js
@@ -6,7 +6,6 @@ function addItem() {
 
   const itemDivNum1 = document.createElement('div')
   itemDivNum1.setAttribute('class', 'items-num')
-  itemDivNum1.setAttribute('id', 'item-'+(count++))
 
   const itemDivNum2 = document.createElement('div')
   itemDivNum1.appendChild(itemDivNum2)
@@ -16,28 +15,45 @@ function addItem() {
   itemDivNum2.appendChild(itemInput)
   itemInput.setAttribute('class', 'item-position')
   itemInput.setAttribute('onblur', 'changeIdNumber(this)')
-  itemInput.value = count-1
 
   const itemP = document.createElement('p')
   itemDivNum2.appendChild(itemP)
   itemP.setAttribute('ondblclick', 'editItem(this)')
   itemP.textContent = inputValue
 
-  const itemSpan = document.createElement('span')
-  itemDivNum1.appendChild(itemSpan)
-  itemSpan.setAttribute('class', 'icon')
-  itemSpan.setAttribute('onclick', 'removeItem(this)')
+  const itemDivNum3 = document.createElement('div')
+  itemDivNum1.appendChild(itemDivNum3)
+  itemDivNum3.setAttribute('class', 'icon')
 
-  const itemIcon = document.createElement('i')
-  itemSpan.appendChild(itemIcon)
-  itemIcon.setAttribute('class', 'fa fa-trash-o')
+  const itemSpan1 = document.createElement('span')
+  itemDivNum3.appendChild(itemSpan1)
+  itemSpan1.setAttribute('onclick', 'checkedItem(this)')
+
+  const itemSpan2 = document.createElement('span')
+  itemDivNum3.appendChild(itemSpan2)
+  itemSpan2.setAttribute('onclick', 'removeItem(this)')
+
+  const itemIcon1 = document.createElement('i')
+  itemSpan1.appendChild(itemIcon1)
+  itemIcon1.setAttribute('class', 'fa fa-check')
+
+  const itemIcon2 = document.createElement('i')
+  itemSpan2.appendChild(itemIcon2)
+  itemIcon2.setAttribute('class', 'fa fa-trash-o')
   
   if(inputValue) {
     document.querySelector('#input-item').value = ''
     listWapper.appendChild(itemDivNum1)
+    itemDivNum1.setAttribute('id', 'item-'+(count++))
+    itemInput.value = count-1
   } else {
     return alert('Please fill out!!!')
   }
+}
+
+//Checked Item
+function checkedItem(checked) {
+  checked.parentNode.previousElementSibling.setAttribute('style', 'text-decoration: line-through;')
 }
 
 // Delete item
@@ -49,6 +65,7 @@ function removeItem(removeElement) {
 function deleteAll() {
   const deleteAllItem = document.querySelector('.list-wapper')
   deleteAllItem.innerHTML = ''
+  count = 1
 }
 
 // Double click to edit content
